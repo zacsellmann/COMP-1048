a85bfe2aeea4b750f711cf5df7a1b51422c10736
zacsellmann <selzy001@mymail.unisa.edu.au>
2025-04-07 20:27:08 +0930

    Add files via upload

---
+++ Calculator attempt.py
@@ -0,0 +1,26 @@
+class Calculator:
+    def add(self, a, b):
+        return a + b
+
+    def subtract(self, a, b):
+        return a - b
+
+    def multiply(self, a, b):
+        return a * b
+
+    def divide(self, a, b):
+        return a / b
+
+
+calc = Calculator()
+print(calc.add(2,41))
+
+calc = Calculator()
+print(calc.subtract(41,2))
+
+calc = Calculator()
+print(calc.multiply(41,2))
+
+calc = Calculator()
+print(calc.divide(41,2))
+
